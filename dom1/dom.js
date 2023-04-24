4e4efe31ac8ea13da027b1753a49e43aa066e558
prashastSINGH <prashast9450@gmail.com>
2023-04-25 03:13:49 +0530

    Signed-off-by: prashastSINGH <prashast9450@gmail.com>

---
+++ dom1/dom.js
@@ -0,0 +1,7 @@
+document.getElementById("header-title").textContent = 'Hello';
+let title = document.getElementById("header-title");
+console.log(title)
+
+let header = document.getElementById("main-header");
+header.style.borderRadius = "20px"
+header.style.borderBottom = "inset 5px black "
