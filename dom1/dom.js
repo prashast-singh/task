986df5e716321f4c8531b29552fd5fc567b1fdba
prashastSINGH <prashast9450@gmail.com>
2023-04-26 09:31:13 +0530

    Signed-off-by: prashastSINGH <prashast9450@gmail.com>

--- dom1/dom.js
+++ dom1/dom.js
@@ -5,3 +5,11 @@ console.log(title)
 let header = document.getElementById("main-header");
 header.style.borderRadius = "20px"
 header.style.borderBottom = "inset 5px black "
+
+let items = document.getElementsByClassName("list-group-item");
+items[0].textContent = 'Hello'
+items[2].style.backgroundColor = "green";
+
+for(var i = 0 ; i< items.length ; i++){
+    items[i].style.fontWeight = "bold";
+}
