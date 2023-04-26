ee062145b6ab72fe1e1bba7bbab835b14883e5cf
prashastSINGH <prashast9450@gmail.com>
2023-04-26 15:09:02 +0530

    Signed-off-by: prashastSINGH <prashast9450@gmail.com>

--- dom1/dom.js
+++ dom1/dom.js
@@ -24,11 +24,79 @@ for(var i = 0 ; i< items.length ; i++){
 
     } */
 
-let item = document.querySelectorAll('.list-group-item');
+/* let item = document.querySelectorAll('.list-group-item');
 
 item[1].style.color = "green";
 
 let odd = document.querySelectorAll('li:nth-child(odd)');
 for(let i =0; i<odd.length; i++)
 odd[i].style.backgroundColor = 'green';
+
+ */
+
+var itemList = document.querySelector('#items');
+console.log(itemList);
+itemList.parentNode.parentNode.parentNode.style.backgroundColor = 'red';
+
+itemList.parentElement.style.backgroundColor = 'green';
+
+console.log(itemList.childNodes);
+console.log(itemList.children);
+
+itemList.children[1].style.backgroundColor = 'yellow';
+
+console.log(itemList.firstChild);
+
+console.log(itemList.firstElementChild);
+itemList.firstElementChild.textContent = 'First Element Child';
+
+console.log(itemList.lastChild)
+
+itemList.lastElementChild.textContent = 'Last Element Child' ;
+itemList.lastElementChild.classList.add("list-group-item");
+
+console.log(itemList.previousSibling);
+console.log(itemList.previousElementSibling);
+
+itemList.nextElementSibling.innerHTML = "<h1>next element sibbling</h1>"
+
+
+var newDiv = document.createElement('div');
+newDiv.className = 'hello';
+newDiv.id = 'hello1';
+
+newDiv.setAttribute('type', 'title');
+console.log(newDiv);
+
+let newText = document.createTextNode('Hello');
+
+newDiv.appendChild(newText)
+
+itemList.appendChild(newDiv)
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('.container h1 ')
+container.insertBefore(newDiv, h1);
+
+item1 = itemList.children[0];
+
+let newText1 = document.createTextNode('Hello');
+
+let newDiv1 = document.createElement('div')
+
+newDiv1.setAttribute('type', 'title');
+
+newDiv1.appendChild(newText1)
+
+
+itemList.insertBefore(newDiv1, item1)
+
+
+
+
+
+
+
+
+
 
