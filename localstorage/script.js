75d8c14d7be0c471b312df30ce826dfb63eb4838
prashastSINGH <prashast9450@gmail.com>
2023-04-27 17:02:00 +0530

    Signed-off-by: prashastSINGH <prashast9450@gmail.com>

--- localstorage/script.js
+++ localstorage/script.js
@@ -5,6 +5,12 @@ form.addEventListener('submit', addItem)
 function addItem(e){
     e.preventDefault();
 
-    localStorage.setItem(form.children[3].value, form.children[1].value)
+    myObj = {
+    name: form.children[1].value,
+    Email : form.children[3].value
+    };
+    let str = JSON.stringify(myObj);
+    
+    localStorage.setItem(form.children[3].value, str);
 
 }
