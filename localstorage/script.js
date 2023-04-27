6a002cd3439a707c5d3bf00f59ee2b3d2644deb4
prashastSINGH <prashast9450@gmail.com>
2023-04-28 01:11:58 +0530

    Signed-off-by: prashastSINGH <prashast9450@gmail.com>

--- localstorage/script.js
+++ localstorage/script.js
@@ -1,6 +1,8 @@
 let form = document.getElementById('addForm');
 let list = document.getElementById('item');
 
+let li = document.querySelectorAll('li')
+
 form.addEventListener('submit', addItem)
 list.addEventListener('click', removeItem)
 
@@ -23,13 +25,21 @@ function addItem(e){
         btn.className = 'btn btn-danger btn-sm float-right delete';
         btn.appendChild(document.createTextNode('Delete'));
 
+
+    let edtbtn = document.createElement('button');
+        edtbtn.className = 'btn btn-danger btn-sm float-right Edit';
+        edtbtn.appendChild(document.createTextNode('Edit'));
+
+
     let key = document.createElement('div');
     key.style.display = 'none' ;
     key.appendChild(document.createTextNode(form.children[3].value));
-    
+
+
     li.appendChild(key);
     li.appendChild(document.createTextNode(form.children[1].value +" - " + form.children[3].value + " - " + form.children[5].value));
     li.appendChild(btn);
+    li.appendChild(edtbtn)
     
     list.appendChild(li);    
 };
@@ -48,5 +58,31 @@ function removeItem(e){
         
         list.removeChild(li);
     }
+
+}
+
+list.addEventListener('click' , edit )
+
+function edit(e){
+e.preventDefault();
+
+
+
+        var li = e.target.parentElement;
+
+        myobjj = JSON.parse(localStorage.getItem(li.firstChild.textContent))
+
+        console.log(myobjj.name)
+
+        document.getElementById("name").value = myobjj.name;
+        document.getElementById("email").value = myobjj.Email;
+        document.getElementById("phone").value = myobjj.Phone;
+
+
+         
+        localStorage.removeItem(li.firstChild.textContent)
+        
+        list.removeChild(li);
+
 
 }
