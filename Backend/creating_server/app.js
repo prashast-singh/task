ef6f2bbd8bd728d8fd4e20bc88ce1cd7d12563bb
prashastSINGH <prashast9450@gmail.com>
2023-05-18 03:23:40 +0530

    Signed-off-by: prashastSINGH <prashast9450@gmail.com>

--- Backend/creating_server/app.js
+++ Backend/creating_server/app.js
@@ -1,6 +1,25 @@
+ 
 const http = require('http')
 const server = http.createServer((req,res)=>{
-    res.write('<body>Prashast</body>')
+    if(req.url==='/home'){
+        res.write('<body>Welcome Home</body>');
+        res.end()
+        return res.end()  
+    }
+
+    if(req.url==='/about'){
+        res.write('<body>Welcome to about us page</body>');
+        return res.end()  
+    }
+
+    if(req.url==='/node'){
+        res.write('<body>Welcome to my Node Js project</body>')
+        return res.end()  
+    }
+   
+    res.write('<body>Default response</body>');
+    res.end();
+    
 })
 
 server.listen(4000);
