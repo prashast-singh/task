6e31f5f025f23f42ef8265a75463dd003007e67c
prashastSINGH <prashast9450@gmail.com>
2023-05-18 01:05:40 +0530

    Signed-off-by: prashastSINGH <prashast9450@gmail.com>

--- Backend/task2/index.js
+++ Backend/task2/index.js
@@ -1,4 +1,27 @@
-const add = (a,b) =>{
+// TASK 1
+
+const fruits =  ['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon'];
+
+console.log(fruits.map(fruit =>{if(fruit===' ')
+                                  return 'empty string'
+
+                                  return fruit
+                                }
+                        )          
+            )
+
+
+ const copiedarray = [...fruits]
+copiedarray.push('arr');
+ console.log(copiedarray)
+ console.log(fruits)
+ // TASK 2
+ 
+
+
+
+
+/* const add = (a,b) =>{
     return a*b;
 }
 
@@ -13,4 +36,4 @@ const Student = {
    },
 };
 
-Student.intro()
+Student.intro() */
