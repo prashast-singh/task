155752997a4d80866a183597244f05e19a2edf5c
prashastSINGH <prashast9450@gmail.com>
2023-05-18 05:57:48 +0530

    Signed-off-by: prashastSINGH <prashast9450@gmail.com>

--- Backend/creating_server/app.js
+++ Backend/creating_server/app.js
@@ -1,31 +1,7 @@
- const fs = require('fs');
 const http = require('http')
-const server = http.createServer((req,res)=>{
-    if(req.url==='/'){
-        const readMessage = fs.readFileSync('./message.txt','utf-8');
-        res.write(`<body><p>${readMessage}</p><form action ="/message" method="POST"><input type="text" name="message"></input><button type ="submit">submit</button></form></body>`);
-        res.end()
-        return res.end()  
-    }
-if(req.url === '/message' && req.method==='POST'){
-    const body = [];
-    req.on('data', (splittedData)=>{
-        body.push(splittedData)
-    })
 
-    req.on('end', ()=>{
-        const parsedBody = Buffer.concat(body).toString();
-        const message = parsedBody.split('=')[1];
-        fs.writeFileSync('./message.txt', message);
-    })
-    
-    res.statusCode = 302;
-    res.setHeader('location','/');
-    return res.end();
-}
-    res.write('<body>Default response</body>');
-    res.end();
-    
-})
+const routes = require('./routes')
+const server = http.createServer(routes.handler)
+console.log(routes.someText)
 
 server.listen(4000);
