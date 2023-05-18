6ecf3c42e6454f8a05c9f8369532e25ab0d902e1
prashastSINGH <prashast9450@gmail.com>
2023-05-19 03:34:15 +0530

    Signed-off-by: prashastSINGH <prashast9450@gmail.com>

--- Backend/creating_server/app.js
+++ Backend/creating_server/app.js
@@ -2,17 +2,28 @@
 const http = require('http')
 
 const express = require('express')
+const bodyParser = require('body-parser')
 
 
 const app = express();
 
-app.use((req, res, next)=>{
-    console.log("1st middleware");
-    next()
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product',(req, res, next)=>{
+    res.send(`<form action = "/product" method="POST"><input type = "text" name="productName"></><input type = "text" name="quantity"></input><button type="submit">SUBMIT</button></form>`)
+    return res.end()
 })
 
-app.use((req, res, next)=>{
-    console.log("in another middleware");
+app.use('/product', (req, res, next)=>{
+console.log(req.body.productName)
+console.log(req.body.quantity)
+res.redirect('/')
+return res.end()
+})
+
+app.use("/", (req, res, next)=>{
+    res.send(`<h1>welcome</h1>`)
+    res.end()
 })
 
 
