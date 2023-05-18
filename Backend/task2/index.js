168bc8961e11184a612f6f9fead54604c463c3b9
prashastSINGH <prashast9450@gmail.com>
2023-05-19 00:21:53 +0530

    Signed-off-by: prashastSINGH <prashast9450@gmail.com>

--- Backend/task2/index.js
+++ Backend/task2/index.js
@@ -16,7 +16,36 @@ copiedarray.push('arr');
  console.log(copiedarray)
  console.log(fruits)
  // TASK 2
+ const print = async ()=>{
+    console.log('a');
+
+    console.log('b');
+
+    const promiseC = new Promise((resolve, reject)=>{
+        setTimeout(() =>{resolve('c')}, 3000);
+    }) ;
+
+    let c = await promiseC;
+     console.log(c);
+
+
+     const promiseD = new Promise((resolve, reject)=>{
+        setTimeout(() =>{console.log('d');  resolve('d')}, 0);
+    }) ;
+
+    let d = await promiseD;
+     
+    
+
+    console.log('e');
+
+ }
  
+
+print()
+
+
+
 
 
 
