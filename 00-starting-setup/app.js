ecb878d4a21f12f8c57ae41edf7df4f495987d8b
prashastSINGH <prashast9450@gmail.com>
2023-05-22 02:40:45 +0530

    Signed-off-by: prashastSINGH <prashast9450@gmail.com>

--- 00-starting-setup/app.js
+++ 00-starting-setup/app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const db = require('./util/database')
+const sequelize = require('./util/database')
 
 const errorController = require('./controllers/error');
 
@@ -24,5 +24,12 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+
+sequelize.sync().then(result =>{
+    console.log(result);
+})
+.catch(err =>{
+    console.log(err)
+});
 
 app.listen(3000);
--- 00-starting-setup/util/database.js
+++ 00-starting-setup/util/database.js
@@ -1,10 +1,9 @@
-const mysql = require('mysql2')
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: '9211'
+const sequelize  = new Sequelize('node-complete', 'root', '9211', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+
+module.exports = sequelize
