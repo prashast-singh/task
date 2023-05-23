f524152418251ffe053e0eb238c11a64c04ce002
prashastSINGH <prashast9450@gmail.com>
2023-05-23 10:29:50 +0530

    Signed-off-by: prashastSINGH <prashast9450@gmail.com>

--- 00-starting-setup/app.js
+++ 00-starting-setup/app.js
@@ -16,7 +16,8 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const Product = require('./models/product')
 const User = require("./models/user")
-
+const Cart = require('./models/cart')
+const CartItem = require('./models/cart-item')
 //return db.execute('SELECT * FROM products WHERE id = 2').then(([rows, fieldData]) =>{console.log(rows[0].id)})
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -36,17 +37,25 @@ app.use(errorController.get404);
 
 Product.belongsTo(User,{constraints: true, onDelete: 'CASCADE'})
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, {through: CartItem});
+Product.belongsToMany(Cart, {through: CartItem}); 
+
 
 sequelize.sync().then(result =>{
     return User.findByPk(1);
-    app.listen(3000);
 }).then(user=>{
     if(!user){
     return  User.create({name: 'Max', email: 'test@test'})
     }
     return user
 }).then(user=>{
-    console.log(user);
+    //console.log(user);
+  return  user.createCart();
+    
+})
+.then(cart=>{
     app.listen(3000);
 })
 .catch(err =>{
